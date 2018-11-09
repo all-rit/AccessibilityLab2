eace80c9a5c2da3f4382097009cb59b1a2093d03
Scott <saf6260@rit.edu>
2018-11-08 23:59:28 -0500

    Attempting to fix circle border issue that occurs with the border flash

--- src/components/home/dropdown.js
+++ src/components/home/dropdown.js
@@ -8,39 +8,44 @@ const Dropdown = ({selectOption}) => {
   }
 
   return (
-    <div className='center'>
-      <select className='selection' onChange={handleChosenOption}>
-        <option 
-          value='default' 
-          className='textSelection'
-        >
-        ---
-        </option>
-        <option 
-          value='hex' 
-          className='textSelection'
-        >
-        Hex Numbers
-        </option>
-        <option 
-          value='Protanopia' 
-          className='textSelection'
-        >
-        Protanopia (Red Blindness)
-        </option>
-        <option 
-          value='Deuteranopia'
-          className='textSelection'
-        >
-          Deuteranopia (Green Blindess)
-        </option>
-        <option 
-          value='Tritanomaly' 
-          className='textSelection'
-        >
-          Tritanomaly (Blue Weakness)
-        </option>
-      </select>
+    <div>
+      <div className='center fourthTitle'>
+        <p>Choose a color option or optional hex display below:</p>
+      </div>
+      <div className='center'>
+        <select className='selection' onChange={handleChosenOption}>
+          <option 
+            value='default' 
+            className='textSelection'
+          >
+            Default
+          </option>
+          <option 
+            value='hex' 
+            className='textSelection'
+          >
+          Hex Numbers
+          </option>
+          <option 
+            value='Protanopia' 
+            className='textSelection'
+          >
+            Protanopia (Red Blindness)
+          </option>
+          <option 
+            value='Deuteranopia'
+            className='textSelection'
+          >
+            Deuteranopia (Green Blindess)
+          </option>
+          <option 
+            value='Tritanomaly' 
+            className='textSelection'
+          >
+            Tritanomaly (Blue Weakness)
+          </option>
+        </select>
+      </div>
     </div>
   );
 }
