4d247e31d9ecc78d4065da824cb845a804ee8651
Scott <saf6260@rit.edu>
2018-11-23 19:16:11 -0500

    Updating file locations and established oauth connection on backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,57 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const auth = require('./auth');
+const key = require('./serverConstants');
 const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+auth(passport);
+app.use(passport.initialize());
+app.use(cookieSession({
+  name: 'session',
+  keys: [key.key]
+}));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+  if (req.session.token) {
+    res.cookie('token', req.session.token);
+    req.json({ 
+      status: 'session cookie set'
+    });
+  } else {
+    res.cookie('token', '')
+    res.json({
+      status: 'session cookie not set'
+    });
+  }
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/'
+  }),
+  (req,res) => {
+    req.session.token = req.user.token;
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', (req, res) => {
+  req.logoit();
+  req.session = null;
+  res.redirect('/');
+});
+
+app.post('/gameStats', (req, res) => {
+  console.log(req);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-app.get('/express_backend', (req, res) => {
-  res.send({express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
-});
--- src/controllers/constants.js
+++
@@ -1,9 +0,0 @@
-export const SELECT_OPTION = "SELECT_OPTION";
-export const RESET_OPTION = "RESET_OPTION";
-
-export const UPDATE_COLORS = "UPDATE_COLORS";
-export const ACTIVATE_POPUP = "ACTIVATE_POPUP";
-export const RESET_COLORS = "RESET_COLORS";
-
-export const START_GAME = "START_GAME";
-export const END_GAME = "END_GAME";
