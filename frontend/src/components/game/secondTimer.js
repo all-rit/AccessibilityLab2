540530083652361775c85f04ef377d943278516f
Scott <saf6260@rit.edu>
2018-12-29 17:10:37 -0500

    Changed declaration from variables to a state delcaration (fixes scoring reset issue)

--- frontend/src/components/game/secondTimer.js
+++ frontend/src/components/game/secondTimer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Score from './score';
 import Instructions from './../instructions/instructions';
 import Circle from './circle';
@@ -6,165 +6,185 @@ import Replay from './replay';
 import Countdown from 'react-countdown-now';
 
 //Component for the secondary timer used for each of the circles per second
-const SecondTimer = ({correctColor, incorrectColorOne, incorrectColorTwo,
-  startTime, gameOption, background, onUpdateTime, selectOption}) => {
+class SecondTimer extends Component {
 
-  let currentColor, correct;
-  let clicked = false;
-  let time = 1;
-  let score = 0;
-  let numRightOnClick = 0;
-  let numRightOnNoClick = 0;
-  let numWrongOnClick = 0;
-  let numWrongOnNoClick = 0;
-  let first = true;
+  //State for score information
+  constructor(props) {
+    super(props);
+    this.currentColor = null;
+    this.correct = null;
+    this.clicked = false;
+    this.time = 1;
+    this.score = 0;
+    this.numRightOnClick = 0;
+    this.numRightOnNoClick = 0;
+    this.numWrongOnClick = 0;
+    this.numWrongOnNoClick = 0;
+    this.first = true;
+  }
 
-  const isHex = (gameOption === 'hex');
+  render() {
+    const {correctColor, incorrectColorOne, incorrectColorTwo, startTime,
+      gameOption, background, onUpdateTime, selectOption} = this.props;
 
-  //Calculates the score based on the response from the user
-  const calculateScore = () => {
-    if (clicked) {
-      if (correct) {
-        numRightOnClick ++;
-        if (time < .1) {
-          score += 100;
-        } else if (time > .1 && time < .21) {
-          score += 75;
-        } else if (time > .2 && time < .31) {
-          score += 50;
-        } else if (time > .3 && time < .41) {
-          score += 35;
-        } else if (time > .4 && time < .51) {
-          score += 25;
-        } else if (time > .5 && time < .61) {
-          score += 15;
-        } else if (time > .6 && time < .71) {
-          score += 10;
+    const isHex = (gameOption === 'hex');
+
+    //Calculates the score based on the response from the user
+    const calculateScore = () => {
+      if (this.numRightOnClick + this.numRightOnNoClick + this.numWrongOnClick +
+      this.numWrongOnNoClick !== 15) {
+        if (this.clicked) {
+          if (this.correct) {
+            this.numRightOnClick ++;
+            if (this.time < .1) {
+              this.score += 100;
+            } else if (this.time > .1 && this.time < .21) {
+              this.score += 75;
+            } else if (this.time > .2 && this.time < .31) {
+              this.score += 50;
+            } else if (this.time > .3 && this.time < .41) {
+              this.score += 35;
+            } else if (this.time > .4 && this.time < .51) {
+              this.score += 25;
+            } else if (this.time > .5 && this.time < .61) {
+              this.score += 15;
+            } else if (this.time > .6 && this.time < .71) {
+              this.score += 10;
+            } else {
+              this.score += 5;
+            }
+          } else {
+            this.numWrongOnClick ++;
+            this.score -= 75;
+          }
+          this.clicked = false;
+          this.time = 1;
         } else {
-          score += 5;
+          if (this.correct) {
+            this.numWrongOnNoClick ++;
+            this.score -= 200;
+          } else {
+            this.numRightOnNoClick ++;
+            this.score += 10;
+          }
         }
-      } else {
-        numWrongOnClick ++;
-        score -= 75;
-      }
-      clicked = false;
-      time = 1;
-    } else {
-      if (correct) {
-        numWrongOnNoClick ++;
-        score -= 200;
-      } else {
-        numRightOnNoClick ++;
-        score += 10;
       }
     }
-  }
 
-  //Randomly generates a color for the middle circle
-  const calculateRandomColor = () => {
-    let options = [correctColor, incorrectColorOne, incorrectColorTwo];
-    let position = Math.floor(Math.random() * (3));
-    currentColor = options[position];
-    return options[position];
-  }
-
-  //Handles the click of the middle circle and records the time
-  const onClick = () => {
-    if(!clicked) {
-      clicked = true;
-      time = ((Date.now() - startTime)/1000) % 1;
+    //Randomly generates a color for the middle circle
+    const calculateRandomColor = () => {
+      let options = [correctColor, incorrectColorOne, incorrectColorTwo];
+      let position = Math.floor(Math.random() * (3));
+      this.currentColor = options[position];
+      return options[position];
     }
-  }
 
-  //turns the data found into an object so it can be passed to the backend
-  //after it is converted, the system sends the info to the backend and then
-  //will record the results from the past five games in the state of the game
-  const recordData = () => {
-    const data = {
-      score,
-      numRightOnClick,
-      numWrongOnClick,
-      numRightOnNoClick,
-      numWrongOnNoClick,
-      Mode: [gameOption.toUpperCase()],
-    };
-
-    const dataJSON = JSON.stringify(data);
-    console.log(dataJSON);
-
-    fetch('http://localhost:5000/gameStats', {
-      method: 'POST',
-      headers: new Headers({'content-type': 'application/json'}),
-      body: JSON.stringify(data)
-    })
-    .then(response => console.log(response))
-    //Need to store response in the controller (redux) and pass to previousGames
-    .catch(err => console.log(err));
-  }
-
-  //Specified by the timer for custom rendering of the center circle
-  const renderer = (props) => {
-    correct = (currentColor === correctColor);
-    calculateRandomColor();
-    if (!first) {
-      calculateScore();
-    } else {
-      first = false;
+    //Handles the click of the middle circle and records the time
+    const onClick = () => {
+      if(!this.clicked) {
+        this.clicked = true;
+        this.time = ((Date.now() - startTime)/1000) % 1;
+      }
     }
-    if (props.total === 0) {
-      recordData();
+
+    //turns the data found into an object so it can be passed to the backend
+    //after it is converted, the system sends the info to the backend and then
+    //will record the results from the past five games in the state of the game
+    const recordData = () => {
+      let score = this.score;
+      let numRightOnClick = this.numRightOnClick;
+      let numWrongOnClick = this.numWrongOnClick;
+      let numRightOnNoClick = this.numRightOnNoClick;
+      let numWrongOnNoClick = this.numWrongOnNoClick;
+      const data = {
+        score,
+        numRightOnClick,
+        numWrongOnClick,
+        numRightOnNoClick,
+        numWrongOnNoClick,
+        Mode: [gameOption.toUpperCase()],
+      };
+
+      const dataJSON = JSON.stringify(data);
+      console.log(dataJSON);
+
+      fetch('http://localhost:5000/gameStats', {
+        method: 'POST',
+        headers: new Headers({'content-type': 'application/json'}),
+        body: JSON.stringify(data)
+      })
+      .then(response => console.log(response))
+      //Need to store response in the controller (redux) and pass to previousGames
+      .catch(err => console.log(err));
     }
-    //Returns either the replay screen option or the center circle that
-    //Changes every second
-    return (
-      <div>
-        {props.total === 0 ?
-        <Replay
-          onUpdateTime={onUpdateTime}
-          score={score}
-          right={numRightOnClick+numRightOnNoClick}
-          wrong={numWrongOnClick+numWrongOnNoClick}
-          selectOption={selectOption}
-        />
-        :
-        <div className='circleClicked'>
-          <Circle
-            clickable={true}
-            color={currentColor}
-            onClick={onClick}
+
+    //Specified by the timer for custom rendering of the center circle
+    const renderer = (props) => {
+      console.log(this.currentColor);
+      console.log(correctColor);
+      this.correct = (this.currentColor === correctColor);
+      console.log(this.correct);
+      calculateRandomColor();
+      if (!this.first) {
+        calculateScore();
+      } else {
+        this.first = false;
+      }
+      if (props.total === 0) {
+        recordData();
+      }
+      //Returns either the replay screen option or the center circle that
+      //Changes every second
+      return (
+        <div>
+          {props.total === 0 ?
+          <Replay
+            onUpdateTime={onUpdateTime}
+            score={this.score}
+            right={this.numRightOnClick+this.numRightOnNoClick}
+            wrong={this.numWrongOnClick+this.numWrongOnNoClick}
+            selectOption={selectOption}
+          />
+          :
+          <div className='circleClicked'>
+            <Circle
+              clickable={true}
+              color={this.currentColor}
+              onClick={onClick}
+            />
+          </div>
+          }
+          <Instructions
+            correctColor={correctColor}
+            incorrectColorOne={incorrectColorOne}
+            incorrectColorTwo={incorrectColorTwo}
+          />
+          <Score
+            score={this.score}
+            rightClick={this.numRightOnClick}
+            wrongClick={this.numWrongOnClick}
+            rightNoClick={this.numRightOnNoClick}
+            wrongNoClick={this.numWrongOnNoClick}
+            isHex={isHex}
+            background={background}
+            currentColor={this.currentColor}
           />
         </div>
-        }
-        <Instructions
-          correctColor={correctColor}
-          incorrectColorOne={incorrectColorOne}
-          incorrectColorTwo={incorrectColorTwo}
-        />
-        <Score
-          score={score}
-          rightClick={numRightOnClick}
-          wrongClick={numWrongOnClick}
-          rightNoClick={numRightOnNoClick}
-          wrongNoClick={numWrongOnNoClick}
-          isHex={isHex}
-          background={background}
-          currentColor={currentColor}
+      );
+    }
+
+    //Used only for the react component used for the countdown system
+    return(
+      <div>
+        <Countdown
+          date={startTime + 18000}
+          intervalDelay={1000}
+          precision={2}
+          renderer={renderer}
         />
       </div>
     );
   }
-
-  //Used only for the react component used for the countdown system
-  return(
-    <div>
-      <Countdown
-        date={startTime + 18000}
-        intervalDelay={1000}
-        precision={2}
-        renderer={renderer}
-      />
-    </div>
-  );
 }
 
 export default SecondTimer;
