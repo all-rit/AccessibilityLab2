9090254e619cb3254522dc80e67c34be5d854804
Scott <saf6260@rit.edu>
2019-01-18 23:02:38 -0500

    Setting up system for server

---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const path= require('path');
+
+let HTMLWebpackPlugin = require('html-webpack-plugin');
+let HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+    template: __dirname + '/public/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  });
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', {
+            'plugins': ['@babel/plugin-proposal-class-properties']}],
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ['css-loader']
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: path.resolve(__dirname + '/build')
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
