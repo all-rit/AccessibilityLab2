6ff9a1b2b817a209e8b42ce9879a0d862a80cf51
Scott <saf6260@rit.edu>
2019-02-05 23:17:38 -0500

    Case studies added to the system and are now stylized, uploaded, and working properly.

--- frontend/src/suppMaterials.js
+++ frontend/src/suppMaterials.js
@@ -1,35 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './suppMaterials.css';
 
 const SuppMaterials = () => {
   return (
     <div>
-      <h1>Supplementary Materials</h1>
-      <p>
-        Slide show is available to view via google slides
-        <a
-          href='https://docs.google.com/presentation/d/1jsPoCObj6a4n9jdxsXQxW3KVHYLJB7rAU2eauUQ4ZCM/edit?usp=sharing'
-          style={{marginLeft: '4px'}}
-        >
-          here
-        </a>
-      </p>
-      <div style={{display: 'flex', justifyContent: 'start'}}>
-        <p>
-          Video is currently unavailable.
+      <h1 className='suppTitle'>Supplementary Materials</h1>
+      <div className='elements'>
+        <p className='suppP'>
+          &#10142; Slide show is available to view via google slides
+          <a
+            href='https://docs.google.com/presentation/d/1jsPoCObj6a4n9jdxsXQxW3KVHYLJB7rAU2eauUQ4ZCM/edit?usp=sharing'
+            style={{marginLeft: '4px'}}
+          >
+            here
+          </a>
         </p>
-        <p style={{marginLeft: '4px'}}>
-          Check back later to watch an interview with individuals who have color vision deficiencies!
+        <div style={{display: 'flex', justifyContent: 'start'}}>
+          <p className='suppP'>
+            &#10142; Video is currently unavailable.
+          </p>
+          <p style={{marginLeft: '4px'}} className='suppP'>
+            Check back later to watch an interview with individuals who have color vision deficiencies!
+          </p>
+        </div>
+        <p className='suppP'>
+          &#10142; Case study is available
+          <span style={{marginLeft: '4px'}}>
+            <Link to='/caseStudy'>
+              here
+            </Link>
+          </span>
+        </p>
+        <p className='suppP'>
+          &#10142; You can also go back to the main application
+          <span style={{marginLeft: '4px'}}>
+            <Link to='/'>
+              here
+            </Link>
+          </span>
         </p>
       </div>
-      <p>
-        Case study is available
-        <span style={{marginLeft: '4px'}}>
-          <Link to='/caseStudy'>
-            here
-          </Link>
-        </span>
-      </p>
     </div>
   );
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -42,6 +42,19 @@ module.exports = (env) => {
           }
         },
         {
+          test:/\.(gif|png|jpe?g|svg)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true,
+                disable: true
+              }
+            }
+          ],
+        },
+        {
           test: /\.(html)$/,
           loader: 'html-loader',
           options: {
