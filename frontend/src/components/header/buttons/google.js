6cb0073f78ed23a6b175546f924236046967d2b1
Saad Khan <sk6786@g.rit.edu>
2020-03-05 18:43:47 -0500

    Removing fluff

--- frontend/src/components/header/buttons/google.js
+++ frontend/src/components/header/buttons/google.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import "../title.css";
-import "../../../../src/assets/stylesheets/components/Header.scss"
+import './../../../assets/stylesheets/components/Header.css';
 /*
 Class for handling the google signin button
 */
