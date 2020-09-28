7c5498562c39a326a7c6aa4bfc6cfacd4f58f66e
cxs7700 <cxs7700@g.rit.edu>
2020-09-28 16:14:58 -0400

    questions

--- frontend/src/quiz/api/quizQuestions.js
+++ frontend/src/quiz/api/quizQuestions.js
@@ -3,35 +3,58 @@ var quizQuestions = [
         question: "What are the most common colors in colorblindedness?",
         answers: [
             {
-                val: 0,
-                content: ""
+                val: 1,
+                content: "Red"
             },
             {
                 val: 1,
-                content: ""
+                content: "Green"
             },
             {
-                val: 0,
-                content: ""
+                val: 1,
+                content: "Blue"
             },
             {
-                val: 0,
-                content: ""
+                val: 1,
+                content: "Grey"
             }
 
         ],
-        multiChoice: false
+        multiChoice: true
     },
     {
         question: "Are women more likely to be affected by a CVD?",
         answers: [
             {
                 val: 0,
+                content: "No."
+            },
+            {
+                val: 0,
+                content: "No"
+            },
+            {
+                val: 1,
+                content: "Yes"
+            },
+            {
+                val: 0,
+                content: "Yes"
+            }
+
+        ],
+        multiChoice: false
+    },
+    {
+        question: "What is a cause of CVDs?",
+        answers: [
+            {
+                val: 0,
                 content: ""
             },
             {
                 val: 1,
-                content: ""
+                content: "Genetic conditions that are usually inherited from an individual's parents."
             },
             {
                 val: 0,
@@ -46,7 +69,7 @@ var quizQuestions = [
         multiChoice: false
     },
     {
-        question: "What is the cure to protanomaly?",
+        question: "What is a major difference between monochromacy and other CVDs?",
         answers: [
             {
                 val: 0,
@@ -54,11 +77,11 @@ var quizQuestions = [
             },
             {
                 val: 1,
-                content: ""
+                content: "Individuals with this deficiency can see no colors at all."
             },
             {
                 val: 0,
-                content: ""
+                content: "Individuals with this deficiency can see only one color."
             },
             {
                 val: 0,
@@ -69,46 +92,23 @@ var quizQuestions = [
         multiChoice: false
     },
     {
-        question: "What is a major difference between  monochromacy and other CVDs?",
+        question: "What, in the eye, causes color to be perceived in the way they are to a person with a CVD?",
         answers: [
+            {
+                val: 0,
+                content: "There is more sensitivity in each of the three cones in the eyes."
+            },
+            {
+                val: 0,
+                content: ""
+            },
             {
                 val: 0,
                 content: ""
             },
             {
                 val: 1,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
-            }
-
-        ],
-        multiChoice: false
-    },
-    {
-        question: "What, in the eye, causes color vision deficiency?",
-        answers: [
-            {
-                val: 0,
-                content: ""
-            },
-            {
-                val: 1,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
-            },
-            {
-                val: 0,
-                content: ""
+                content: "Individuals with a CVD have only two types of cones to perceive colors."
             }
 
         ],
